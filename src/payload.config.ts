53770985e851d4f9f45e67424bb37922e48f7b78
zacmorehouselogin@gmail.com <zacmorehouselogin@gmail.com>
2025-08-12 15:48:12 +1000

    Migrated to PostgreSQL

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,5 +1,5 @@
 // storage-adapter-import-placeholder
-import { sqliteAdapter } from '@payloadcms/db-sqlite'
+import { postgresAdapter } from '@payloadcms/db-postgres'
 import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
@@ -27,9 +27,9 @@ export default buildConfig({
   typescript: {
     outputFile: path.resolve(dirname, 'payload-types.ts'),
   },
-  db: sqliteAdapter({
-    client: {
-      url: process.env.DATABASE_URI || '',
+  db: postgresAdapter({
+    pool: {
+      connectionString: process.env.DATABASE_URL!,
     },
   }),
   sharp,
